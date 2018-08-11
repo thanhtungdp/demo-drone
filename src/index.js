af5c4b71913b670ac6e630e80337c4116b456ca2
thanhtungdp <tungptkh@gmail.com>
2018-08-11 13:43:12 +0700

    udpate cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const { router, get, withNamespace } = require('microrouter')
 const { handleErrors } = require('@bit/tungtung.micro.components.micro-boom')
 const createMiddlewareAuth = require('@bit/tungtung.micro.components.middleware-auth')
+const cors = require('micro-cors')({ origin: '*' })
 const config = require('config')
 // const faker = require('faker')
 
@@ -8,9 +9,10 @@ const namespace = withNamespace(`/${config.serviceName}`)
 
 const authMiddleware = createMiddlewareAuth()
 
+const routerConbine = (...args) => cors(router(...args))
 const composeMiddle = func => handleErrors(authMiddleware(func))
 
-module.exports = router(
+module.exports = routerConbine(
   namespace(
     get(
       '/user',
