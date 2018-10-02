238a7b063fb29854a959d2d8c65c6fc9ae13cc24
Trong Tran <trongtran19021997@gmail.com>
2018-10-02 10:28:55 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { router, get, withNamespace } = require('microrouter')
 const { handleErrors } = require('@bit/tungtung.micro.components.micro-boom')
-const createMiddlewareAuth = require('@bit/tungtung.micro.components.middleware-auth')
 const playerRouter = require('./routes/player')
 const cors = require('micro-cors')({ origin: '*' })
 const config = require('config')
@@ -8,10 +7,7 @@ const config = require('config')
 
 const namespace = withNamespace(`/${config.serviceName}`)
 
-const authMiddleware = createMiddlewareAuth()
-
 const routerConbine = (...args) => cors(router(...args))
-const composeMiddle = func => handleErrors(authMiddleware(func))
 
 module.exports = routerConbine(
   namespace(
