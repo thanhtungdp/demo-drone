0921637e895f3ad32701ede1cbbb570f6ebc0811
Tien Nguyen <toantien.vn91@gmail.com>
2018-11-07 12:05:41 +0700

    Add AMQP

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
   serviceName: 'test',
-  mongodbUrl: process.env.MONGODB_URL
+  mongodbUrl: process.env.MONGODB_URL,
+  publicApp: `${process.env.SUPPORTED_PROTOCOL}://${process.env.DOMAIN_NAME}`,
+  publicAppProtocol: process.env.PUBLIC_APP_PROTOCOL,
+  AMQP: process.env.AMQP,
 }
